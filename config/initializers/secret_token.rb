4e9737e0194b59d993651e1b0e516463df970be1
Mark Campbell <nitrodist@gmail.com>
2012-04-12 17:46:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assentworks::Application.config.secret_token = '01c0f1e9fbdac6afd04757fcc1721bbfbbb45294bcb002949a6690ccb49742a081230e1c15dfd0ec686c916ac3fcf4003d5f2917f683eb041b61eccf25ee67c5'
